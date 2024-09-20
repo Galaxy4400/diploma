6a504cfedc963d5bc6735f24c4d850ca9455a7b3
Moiseev Evgeny <moiseevEO@yandex.ru>
2024-09-20 20:25:09 +0300

    commit

--- src/features/user/user-edit/user-edit.rules.js
+++ src/features/user/user-edit/user-edit.rules.js
@@ -1,15 +1,22 @@
 import * as yup from 'yup';
 
+const loginRules = yup.string().required().min(3).max(15);
+
+const passwordRules = yup.string()
+	.transform((value) => !value ? undefined : value)
+	.when('password', {
+		is: (value) => value?.length,
+		then: (schema) => schema.min(3).max(15),
+		otherwise: (schema) => schema.notRequired(),
+	});
+
+const passcheckRules = yup.string()
+	.transform((value) => !value ? undefined : value)
+	.oneOf([yup.ref('password'), null]);
+
+	
 export const editUserFormRules = yup.object().shape({
-	login: yup.string().required().min(3).max(15),
-	password: yup.string()
-		.transform((value) => !value ? undefined : value)
-		.when('password', {
-			is: (value) => value?.length,
-			then: (schema) => schema.min(3).max(15),
-			otherwise: (schema) => schema.notRequired(),
-		}),
-	passcheck: yup.string()
-		.transform((value) => !value ? undefined : value)
-		.oneOf([yup.ref('password'), null]),
+	login: loginRules,
+	password: passwordRules,
+	passcheck: passcheckRules,
 }, [['password', 'password']]);
