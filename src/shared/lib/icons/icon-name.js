0416c362c266b60925389d250fb739876a2755d6
Moiseev Evgeny <moiseevEO@yandex.ru>
2024-09-21 14:49:53 +0300

    commit

---
+++ src/shared/lib/icons/icon-name.js
@@ -0,0 +1,6 @@
+export const ICON_NAME = {
+	ICON1: 'icon1',
+	ICON2: 'icon2',
+	ICON3: 'icon3',
+	ICON4: 'icon4',
+};
--- src/shared/ui/icon/icon.jsx
+++ src/shared/ui/icon/icon.jsx
@@ -1,14 +1,16 @@
+import { ICON_NAME } from "../../lib/icons";
 import Icon1 from "./icons/icon1.svg?react";
 import Icon2 from "./icons/icon2.svg?react";
 import Icon3 from "./icons/icon3.svg?react";
 import Icon4 from "./icons/icon4.svg?react";
 
+
 export const Icon = ({ name, className, size, ...rest }) => {
 	switch (name) {
-		case 'icon1': return <Icon1 className={className} width={size} height={size} {...rest} />
-		case 'icon2': return <Icon2 className={className} width={size} height={size} {...rest} />
-		case 'icon3': return <Icon3 className={className} width={size} height={size} {...rest} />
-		case 'icon4': return <Icon4 className={className} width={size} height={size} {...rest} />
+		case ICON_NAME.ICON1: return <Icon1 className={className} width={size} height={size} {...rest} />
+		case ICON_NAME.ICON2: return <Icon2 className={className} width={size} height={size} {...rest} />
+		case ICON_NAME.ICON3: return <Icon3 className={className} width={size} height={size} {...rest} />
+		case ICON_NAME.ICON4: return <Icon4 className={className} width={size} height={size} {...rest} />
 
 		default:
 			break;
