98662c51fb13a3ac2373c7c3f363e71b35dee764
Moiseev Evgeny <moiseevEO@yandex.ru>
2024-09-22 22:25:55 +0300

    commit

--- src/features/category/category-edit/category-edit.ui.jsx
+++ src/features/category/category-edit/category-edit.ui.jsx
@@ -2,7 +2,7 @@ import { categoryEditFormRules } from "./category-edit.rules";
 import { useDispatch } from "react-redux";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { CATEGORY_TYPES, setCategory } from "../../../entities/category";
-import { useNavigate } from "react-router-dom";
+import { useAsyncValue, useNavigate } from "react-router-dom";
 import { path } from "../../../shared/lib/router";
 import { Form, Input, Radio, RadioComponent } from "../../../shared/ui/react-hook-form";
 import { useState } from "react";
@@ -12,45 +12,42 @@ import { CATEGORY_ICON } from "../../../shared/lib/icons";
 import { server } from "../../../shared/bff";
 
 
-export const CategoryEditForm = ({ categoryData }) => {
+export const CategoryEditForm = () => {
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
+	const category = useAsyncValue();
 	const [isLoading, setIsLoading] = useState(false);
 
 	const submitHandler = async (submittedData) => {
 		setIsLoading(true);
 
-		const { data: category } = await server.updateCategory(categoryData.id, submittedData);
+		const response = await server.updateCategory(category.id, submittedData);
 
-		dispatch(setCategory(category));
+		dispatch(setCategory(response.data));
 
 		setIsLoading(false);
 
-		navigate(path.category.id(categoryData.id));
+		navigate(path.category.id(category.id));
 	};
 
 	return (
-		<>
-			{categoryData &&
-				<Loader isLoading={isLoading} style={{ width: "300px" }}>
-					<Form onSubmit={submitHandler} resolver={yupResolver(categoryEditFormRules)} style={{ display: "grid", gap: "10px" }}>
-						<Input name="name" defaultValue={categoryData.name} placeholder='Название категории...' />
-						<div>
-							{CATEGORY_TYPES.map((type) => (
-								<Radio key={type.id} name="typeId" value={type.id} label={type.name} defaultChecked={type.id === categoryData.typeId} />
-							))}
-						</div>
-						<div style={{display: 'flex', gap: '10px'}}>
-							{Object.values(CATEGORY_ICON).map((icon) => (
-								<RadioComponent key={icon} name="icon" value={icon} defaultChecked={icon === categoryData.icon}>
-									<CategoryIcon name={icon} />
-								</RadioComponent>
-							))}
-						</div>
-						<button type='submit'>Внести изменения</button>
-					</Form>
-				</Loader>
-			}
-		</>
+		<Loader isLoading={isLoading} style={{ width: "300px" }}>
+			<Form onSubmit={submitHandler} resolver={yupResolver(categoryEditFormRules)} style={{ display: "grid", gap: "10px" }}>
+				<Input name="name" defaultValue={category.name} placeholder='Название категории...' />
+				<div>
+					{CATEGORY_TYPES.map((type) => (
+						<Radio key={type.id} name="typeId" value={type.id} label={type.name} defaultChecked={type.id === category.typeId} />
+					))}
+				</div>
+				<div style={{display: 'flex', gap: '10px'}}>
+					{Object.values(CATEGORY_ICON).map((icon) => (
+						<RadioComponent key={icon} name="icon" value={icon} defaultChecked={icon === category.icon}>
+							<CategoryIcon name={icon} />
+						</RadioComponent>
+					))}
+				</div>
+				<button type='submit'>Внести изменения</button>
+			</Form>
+		</Loader>
 	);
 };
--- src/pages/category-edit/ui/category-edit-page.ui.jsx
+++ src/pages/category-edit/ui/category-edit-page.ui.jsx
@@ -1,14 +1,15 @@
 import { useLoaderData } from "react-router-dom";
 import { CategoryEditForm } from "../../../features/category";
+import { AsyncComponent } from "../../../shared/ui";
 
 
 export const CategoryEditPage = () => {
-	const category = useLoaderData();
+	const { category } = useLoaderData();
 
 	return (
 		<div>
-			<h1>СТРАНИЦА РЕДАКТИРОВАНИЯ СЧЕТА</h1>
-			<CategoryEditForm categoryData={category} />
+			<h1>СТРАНИЦА РЕДАКТИРОВАНИЯ КАТЕГОРИИ</h1>
+			<AsyncComponent resolve={category} element={<CategoryEditForm />} fallback={<div>Загрузка данных...</div>} />
 		</div>
 	)
 };
