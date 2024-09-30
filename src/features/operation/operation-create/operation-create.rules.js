99b812f868cd8e8a9abe62c53adfc16b2f1a5ba5
Moiseev Evgeny <moiseevEO@yandex.ru>
2024-09-30 19:47:58 +0300

    commit

--- src/features/operation/operation-create/operation-create.rules.js
+++ src/features/operation/operation-create/operation-create.rules.js
@@ -2,8 +2,8 @@ import * as yup from 'yup';
 
 export const operationCreateFormRules = yup.object().shape({
 	userId: yup.number().required(),
-	name: yup.string().required().min(3).max(30),
 	amount: yup.number().required(),
 	categoryId: yup.number().required(),
 	accountId: yup.number().required(),
+	comment: yup.string(),
 });
