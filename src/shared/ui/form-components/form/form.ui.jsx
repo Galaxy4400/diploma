997cf6f98580c0666cb5c1bb3ca5ca20683857ac
Moiseev Evgeny <moiseevEO@yandex.ru>
2024-10-06 17:36:29 +0300

    commit

--- src/shared/ui/form-components/form/form.ui.jsx
+++ src/shared/ui/form-components/form/form.ui.jsx
@@ -1,18 +1,13 @@
 import { FormProvider, useForm } from 'react-hook-form';
-import { ErrorList } from '../../components/error-list';
 
 export function Form({ className = '', defaultValues, resolver, onSubmit, children, ...rest }) {
 	const methods = useForm({ defaultValues, resolver });
-	const {
-		handleSubmit,
-		formState: { errors },
-	} = methods; // TODO перенести обработку ошибок в другое место
+	const { handleSubmit } = methods;
 
 	return (
 		<FormProvider {...{ ...methods, onSubmit }}>
 			<form className={className} onSubmit={handleSubmit(onSubmit)} {...rest}>
 				{children}
-				{<ErrorList formErrors={errors} />} {/* TODO перенести обработку ошибок в другое место */}
 			</form>
 		</FormProvider>
 	);
--- src/shared/ui/form-components/input/input.ui.jsx
+++ src/shared/ui/form-components/input/input.ui.jsx
@@ -2,12 +2,16 @@ import css from './input.module.scss';
 import { useFormContext } from 'react-hook-form';
 
 export function Input({ name, label, type = 'text', ...rest }) {
-	const { register } = useFormContext();
+	const {
+		register,
+		formState: { errors },
+	} = useFormContext();
 
 	return (
 		<label className={css['wraper']}>
 			{label && <span className={css['label']}>{label}</span>}
 			<input className={css['input']} {...register(name)} type={type} {...rest} />
+			{!!errors[name] && <p className={css['error']}>{errors[name]?.message}</p>}
 		</label>
 	);
 }
