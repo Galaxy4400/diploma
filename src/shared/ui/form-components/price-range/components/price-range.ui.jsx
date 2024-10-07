0a09d7afc9bbcde812d6b6bc4613e0f4a1ce77bf
Moiseev Evgeny <moiseevEO@yandex.ru>
2024-10-07 20:08:17 +0300

    commit

--- src/shared/ui/form-components/price-range/components/price-range.ui.jsx
+++ src/shared/ui/form-components/price-range/components/price-range.ui.jsx
@@ -1,11 +1,15 @@
-import { useState } from 'react';
 import css from './price-range.module.scss';
+import { useEffect, useRef, useState } from 'react';
+import cn from 'classnames';
+import { priceFormat } from '../../../../lib/utils/price-format';
 
 export const PriceRange = ({ lowPrice: initialLowPrice, highPrice: initialHighPrice, onChange }) => {
 	const [lowPrice, setLowPrice] = useState(initialLowPrice || '0');
 	const [highPrice, setHighPrice] = useState(initialHighPrice || '0');
+	const [isActive, setIsActive] = useState(false);
+	const wrapperRef = useRef(null);
 
-	const display = [lowPrice, highPrice].join(' - ');
+	const display = [priceFormat(lowPrice), priceFormat(highPrice)].join(' - ');
 
 	const lowPriceChangeHandler = (event) => {
 		setLowPrice(event.target.value);
@@ -17,17 +21,40 @@ export const PriceRange = ({ lowPrice: initialLowPrice, highPrice: initialHighPr
 		onChange([lowPrice, highPrice]);
 	};
 
+	const handleClickInside = () => {
+		setIsActive(true);
+	};
+
+	const handleClickOutside = (event) => {
+		if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+			setIsActive(false);
+		}
+	};
+
+	useEffect(() => {
+		document.addEventListener('mousedown', handleClickOutside);
+
+		return () => document.removeEventListener('mousedown', handleClickOutside);
+	}, []);
+
 	return (
-		<div className={css['wrapper']}>
-			<input className={css['input']} type="text" value={display} readOnly placeholder="placeholder" />
-			<div>
+		<div className={css['wrapper']} ref={wrapperRef}>
+			<input
+				className={css['input']}
+				onClick={handleClickInside}
+				type="text"
+				value={display}
+				readOnly
+				placeholder="placeholder"
+			/>
+			<div className={cn(css['container'], isActive ? 'active' : '')}>
 				<label>
-					<span>Low price</span>
-					<input type="text" value={lowPrice} onChange={lowPriceChangeHandler} />
+					<span>От</span>
+					<input className={css['input']} type="number" value={lowPrice} onChange={lowPriceChangeHandler} />
 				</label>
 				<label>
-					<span>High price</span>
-					<input type="text" value={highPrice} onChange={highPriceChangeHandler} />
+					<span>До</span>
+					<input className={css['input']} type="number" value={highPrice} onChange={highPriceChangeHandler} />
 				</label>
 			</div>
 		</div>
