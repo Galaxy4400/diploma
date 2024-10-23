46816abc8dcea155958106df11711d6cc3ead5d4
Moiseev Evgeny <moiseevEO@yandex.ru>
2024-10-23 16:45:04 +0300

    commit

--- forntend/src/app/main.tsx
+++ forntend/src/app/main.tsx
@@ -2,10 +2,10 @@ import { createRoot } from 'react-dom/client';
 import { App } from './app';
 import { registerLocale } from 'react-datepicker';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
-import ru from 'date-fns/locale/ru';
+import { ru } from 'date-fns/locale';
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 registerLocale('ru', ru);
 
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById('root')!).render(<App />);
