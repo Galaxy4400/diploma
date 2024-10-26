7a859c420ee7a0d8d9035330fa551ef89c662d3f
Moiseev Evgeny <moiseevEO@yandex.ru>
2024-10-26 19:13:44 +0300

    commit

--- forntend/src/pages/account-edit/lib/account-edit-page.loader.ts
+++ forntend/src/pages/account-edit/lib/account-edit-page.loader.ts
@@ -1,14 +1,20 @@
+import { AccountResponse } from 'entities/account';
 import { defer } from 'react-router-dom';
-import { request } from '../../../shared/api';
+import { request } from 'shared/api';
+import { HasParams, ID } from 'shared/types';
 
-const getAccount = async (accountId) => {
-	const { account } = await request({ url: `/accounts/${accountId}` });
+const getAccount = async (accountId: ID) => {
+	const { account } = await request<AccountResponse>({ url: `/accounts/${accountId}` });
 
 	return account;
 };
 
-export const accountEditPageLoader = async ({ params }) => {
+export const accountEditPageLoader = async ({ params }: HasParams<{ id: string }>) => {
 	const id = params.id;
+
+	if (!id) {
+		throw new Error('Account ID is missing in route parameters.');
+	}
 
 	return defer({
 		account: getAccount(id),
--- forntend/src/pages/account-edit/ui/account-edit-page.ui.tsx
+++ forntend/src/pages/account-edit/ui/account-edit-page.ui.tsx
@@ -1,11 +1,18 @@
 import { useLoaderData } from 'react-router-dom';
-import { AccountEditForm } from '../../../features/account';
-import { AsyncComponent, Loading } from '../../../shared/ui/components';
-import { Container } from '../../../shared/ui/components';
-import { PageHeader } from '../../../widgets/page-header';
+import { AccountEditForm } from 'features/account';
+import { AsyncComponent, Loading } from 'shared/ui/components';
+import { Container } from 'shared/ui/components';
+import { PageHeader } from 'widgets/page-header';
+import { ID } from 'shared/types';
+import { AccountType } from 'entities/account';
+
+interface AccountEditPageLoaderData {
+	id: ID;
+	account: Promise<AccountType>;
+}
 
 export const AccountEditPage = () => {
-	const { account } = useLoaderData();
+	const { account } = useLoaderData() as AccountEditPageLoaderData;
 
 	return (
 		<Container>
