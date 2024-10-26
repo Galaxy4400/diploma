5d26f4346b152f09012ec78b76dcebf8ce390dc7
Moiseev Evgeny <moiseevEO@yandex.ru>
2024-10-26 15:24:17 +0300

    commit

--- forntend/src/pages/account-create/account-create-page.ui.jsx
+++ forntend/src/pages/account-create/account-create-page.ui.jsx
@@ -1,5 +1,3 @@
-import { useSelector } from 'react-redux';
-import { selectAuthId } from '../../entities/auth';
 import { AccountCreateForm } from '../../features/account';
 import { Container } from '../../shared/ui/components';
 import { PageHeader } from '../../widgets/page-header';
