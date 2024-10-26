1bf378b4502b68207dd0e3ccfb5d167fc9344cd5
Moiseev Evgeny <moiseevEO@yandex.ru>
2024-10-27 01:50:15 +0300

    commit

--- forntend/src/features/operation/operation-delete/operation-delete.ui.tsx
+++ forntend/src/features/operation/operation-delete/operation-delete.ui.tsx
@@ -1,26 +1,25 @@
 import css from './operation-delete.module.scss';
 import { useNavigate } from 'react-router-dom';
-import { useFrom } from '../../../shared/lib/location';
-import { Icon } from '../../../shared/ui/icons';
-import { useModal } from '../../../app/providers/modal';
-import { Confirm } from '../../../shared/ui/components';
-import { request } from '../../../shared/api';
-import { useToast } from '../../../app/providers/toast';
-import { Icons } from 'shared/types';
+import { useFrom } from 'shared/lib/location';
+import { Icon } from 'shared/ui/icons';
+import { useModal } from 'app/providers/modal';
+import { Confirm } from 'shared/ui/components';
+import { request } from 'shared/api';
+import { useToast } from 'app/providers/toast';
+import { Icons, ID } from 'shared/types';
 
-export const OperationDelete = ({ operationId }) => {
+interface OperationDeleteProps {
+	operationId: ID;
+}
+
+export const OperationDelete = ({ operationId }: OperationDeleteProps) => {
 	const navigate = useNavigate();
 	const { showToast } = useToast();
 	const from = useFrom();
 	const { openModal, closeModal } = useModal();
 
 	const deleteOperation = async () => {
-		const { error } = await request({ url: `/operations/${operationId}`, method: 'DELETE' });
-
-		if (error) {
-			showToast({ message: 'Ошибка! Попробуйте ещё раз', type: 'error' });
-			return;
-		}
+		await request({ url: `/operations/${operationId}`, method: 'DELETE' });
 
 		navigate(from?.pathname || false, { replace: true });
 
