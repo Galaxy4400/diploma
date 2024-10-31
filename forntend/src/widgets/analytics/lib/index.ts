ca3bb5097258db0139b0f0e54f067bc84b831d54
Moiseev Evgeny <moiseevEO@yandex.ru>
2024-11-01 02:41:42 +0300

    commit

--- forntend/src/widgets/analytics/lib/index.ts
+++ forntend/src/widgets/analytics/lib/index.ts
@@ -1,4 +1,5 @@
+export * from './get-chart-data';
 export * from './range-type-options';
 export * from './get-week-name';
-export * from './chart.types';
 export * from './chart.options';
+export * from './chart.types';
